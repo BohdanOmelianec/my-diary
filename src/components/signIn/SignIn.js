18f8baac2c250cfd7991d3c9e3a373c0bb423a86
Bohdan Omelianets <omelianecb@gmail.com>
2021-10-21 17:07:40 +0300

    fix

--- src/components/signIn/SignIn.js
+++ src/components/signIn/SignIn.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Link, Redirect } from 'react-router-dom';
 import logo from '../../img/signin_logo.svg';
